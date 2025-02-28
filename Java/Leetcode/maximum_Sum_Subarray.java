5352cae82d0070ddc2aa483c03ee43c6640e9429
Phanindra <phanindrajaychand0@gmail.com>
2025-02-28 10:19:30 +0530

    Updated

---
+++ Java/Leetcode/maximum_Sum_Subarray.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+public class maximum_Sum_Subarray {
+    public static int maxSumSubarrayWithDistinctElements(int[] arr, int k) {
+        int n = arr.length;
+        if (n<k) {
+            return 0;
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0;i<=n-k;i++){
+            Set<Integer> windowSet = new HashSet<>();
+            int currentSum = 0;
+            for(int j=i;j<i+k;j++){
+                if (!windowSet.add(arr[j])) {
+                    currentSum = -1;
+                    break;
+                }
+                currentSum+=arr[j];
+            }
+            if (currentSum!=-1){
+                maxSum = Math.max(maxSum, currentSum);
+            }
+           // System.out.println(windowSet);
+        }
+         return maxSum;      
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 4, 2, 9, 9, 9};
+        int k = 3;
+        int result = maxSumSubarrayWithDistinctElements(arr, k);
+        System.out.println(result);
+    }     
+}
