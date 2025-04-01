e804818852841d1b54887cbcd40030623e7a379f
Phanindra <phanindrajaychand0@gmail.com>
2025-04-01 11:40:58 +0530

    Update

---
+++ Core-java/java/J00N/J018.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class J018 {
+    static int a = 67;
+    Scanner in = new Scanner(System.in);
+
+    public boolean m1(String s) {
+        System.out.println("Calling M1");
+        return in.nextBoolean();
+    }
+
+    public static void m2(int a) {
+        System.out.println("Calling M2");
+        obm1(String s);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
