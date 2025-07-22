8478850a435d155a87a4e3a6828da18fd58488bb
phanindrajaychand <phanindrajaychand0@gmail.com>
2025-07-22 15:41:06 +0530

    Changes...

--- Core-java/Collections/Sets/TrianglePattern.java
+++ Core-java/Collections/Sets/TrianglePattern.java
@@ -8,8 +8,19 @@ public class TrianglePattern {
             for (int j = 1; j <= stars; j++) {
                 System.out.print("*");
             }
-
+            
             System.out.println();
         }
+
+    // for (int i = 1,j=1; i <= 2 * n - 1; j++) {
+    //     System.out.print("0");
+    //     if(i==j || 2*n-j==i)
+    //     {
+    //         System.out.println();
+    //         i++;
+    //         j=0;
+    //     }
+    // }
+
     }
 }
