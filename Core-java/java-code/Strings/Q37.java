598e1504bba20668327ebce25e1e651bba7d3185
phanindrajaychand <phanindrajaychand0@gmail.com>
2025-07-29 16:10:00 +0530

    changes...

---
+++ Core-java/java-code/Strings/Q37.java
@@ -0,0 +1,20 @@
+public class Q37 {
+    public static void main(String[] args) {
+        String s = "Hii hello Hii jj ww er aalh jdnf";
+        String[] a = s.split(" ");
+        boolean[] b = new boolean[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            if (!b[i]) {
+                int count = 1;
+                for (int j = i + 1; j < a.length; j++) {
+                    if (a[i].equals(a[j])) {
+                        count++;
+                        b[j] = true;
+                    }
+                }
+                System.out.println(a[i] + " " + count);
+            }
+        }
+    }
+}
--- FrontEnd/Html/work-2/script.js
+++ FrontEnd/Html/work-2/script.js
@@ -1,9 +1,16 @@
 function validationForm() {
     let x = document.forms["myForm"]["uname"].value.trim(); 
     let y = document.forms["myForm"]["password"].value;
+
     if (x === "" || y === "") {
-        alert("Enter name and password");
+        alert("Please enter both username and password.");
         return false;
     }
+
+    if (y.length < 2 || y.length > 8) {
+        alert("Password must be between 2 and 8 characters long.");
+        return false;
+    }
+
     return true;
 }
