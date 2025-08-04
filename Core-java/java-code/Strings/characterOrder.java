4ef7edaeededfffc3ed347786959667196885c63
phanindrajaychand <phanindrajaychand0@gmail.com>
2025-08-04 16:21:28 +0530

    changes...

--- Core-java/java-code/Strings/characterOrder.java
+++ Core-java/java-code/Strings/characterOrder.java
@@ -8,7 +8,13 @@ public class characterOrder {
     public static void sortWordsAlphabetically(String str) {
         String[] words = str.split(" ");
 
-l                }
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words.length - 1 - i; j++) {
+                if (words[j].compareToIgnoreCase(words[j + 1]) > 0) {
+                    String temp = words[j];
+                    words[j] = words[j + 1];
+                    words[j + 1] = temp;
+                }
             }
         }
         for (String word : words) {
